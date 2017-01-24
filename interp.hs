6b67596a8629c93d3a89ecadfb3cafaf082dd24e
Chris <chris.cornelison@gmail.com>
2017-01-23 21:08:12 -0600

    update

--- interp.hs
+++ interp.hs
@@ -4,58 +4,73 @@ import Prelude hiding (lookup)
 type Name = String
 
 data Term = Var Name
-          | Con Int
+          | Const Int
           | Add Term Term
           | Lam Name Term
           | App Term Term
+          | Count
 
 data Value = Wrong
            | Num Int
-           | Fun (Value -> M Value)
+           | Fun (Value -> S Value) 
 
 type Environment = [(Name, Value)]
 
-data M a = M a
+type State = Int
+type S a = State -> (a, State)
 
-unitM :: a -> M a
-unitM = M 
+interp :: Term -> Environment -> S Value
+interp (Var x) e  = lookup x e
+interp (Const i) e  = unitS (Num i)
+interp (Add u v) e = interp u e `bindS` (\a ->
+                     interp v e `bindS` (\b ->
+                     add a b))
+interp (Lam x v) e = unitS (Fun (\a -> interp v ((x,a):e)))
+interp (App t u) e = interp t e `bindS` (\f ->
+                     interp u e `bindS` (\a ->
+                     apply f a))
+interp Count e = fetchS `bindS` (\i -> unitS (Num i))
 
-bindM :: M a -> (a -> M b) -> M b
-bindM (M x) f = f x
+add :: Value -> Value -> S Value
+add (Num i) (Num j) = tickS `bindS` (\() -> unitS (Num (i+j)))
+add a b             = unitS Wrong
 
-showM :: M Value -> String
-showM (M x) = showval x
+apply :: Value -> Value -> S Value
+apply (Fun k) a = tickS `bindS` (\() -> k a)
+apply f a       = unitS Wrong
+
+unitS :: a -> State -> (a, State)
+unitS a s0 = (a, s0)
+
+tickS :: S ()
+tickS s = ((), s+1)
+
+bindS :: S a -> (a -> S b) -> S b
+m `bindS` k = \s0 -> let (a,s1) = m s0
+                         (b,s2) = k a s1
+                     in  (b,s2)
+
+showS :: S Value -> String
+showS m = let (a,s1) = m 0
+          in "Value: " ++ showval a ++ "; " ++
+             "Count: " ++ show s1
 
 showval :: Value -> String
 showval Wrong   = "<wrong>"
 showval (Num i) = show i
 showval (Fun f) = "<function>"
 
-lookup :: Name -> Environment -> M Value
-lookup x []        = unitM Wrong
-lookup x ((y,b):e) = if x == y then unitM b else lookup x e
+fetchS :: S State
+fetchS s = (s, s)
 
-interp :: Term -> Environment -> M Value
-interp (Var x) e  = lookup x e
-interp (Con i) e  = unitM (Num i)
-interp (Add u v) e = interp u e `bindM` (\a ->
-                     interp v e `bindM` (\b ->
-                     add a b))
-interp (Lam x v) e = unitM (Fun (\a -> interp v ((x,a):e)))
-interp (App t u) e = interp t e `bindM` (\f ->
-                     interp u e `bindM` (\a ->
-                     apply f a))
 
-add :: Value -> Value -> M Value
-add (Num i) (Num j) = unitM (Num (i+j))
-add a b             = unitM Wrong
 
-apply :: Value -> Value -> M Value
-apply (Fun k) a = k a
-apply f a       = unitM Wrong
+lookup :: Name -> Environment -> S Value
+lookup x []        = unitS Wrong
+lookup x ((y,b):e) = if x == y then unitS b else lookup x e
 
 test :: Term -> String
-test t = showM (interp t [])
+test t = showS (interp t [])
 
 term0 = (App (Lam "x" (Add (Var "x") (Var "x")))
-             (Add (Con 10) (Con 11)))
+             (Add (Const 10) (Const 11)))
